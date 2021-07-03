5404e3506a95c25b52a0e40c2701c2d57032ecdd
kritikakarri <kkarri@bu.edu>
2021-07-03 09:04:21 -0400

    Update Functions.R

--- R/Functions.R
+++ R/Functions.R
@@ -2481,7 +2481,7 @@ colnames(Df)=gene.names
 
 print(sprintf('Inferred the raw regulatory network: %g nodes and %g edges (ratio E/N)=%f',length(igraph::V(G)),length(igraph::E(G)),length(igraph::E(G))/length(igraph::V(G))))
 
-G=polish.graph(G)
+G=polish.graph(G,1)
 comp=igraph::components(G)
 small.comp=which(comp$csize<0.01*sum(comp$csize))
 to.remove=which(is.element(comp$membership,small.comp))
